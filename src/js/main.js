8a82939935b80b89074d57913a80c02685eb00b6
Eeemlo <emma.lorensson@outlook.com>
2024-05-23 15:26:41 +0200

    Lagt till omdirigering från menyknapp

--- src/js/main.js
+++ src/js/main.js
@@ -84,3 +84,10 @@ const bookAnimation = document.querySelector(".book");
 bookAnimation.addEventListener("click", () => {
     window.location.href = "/book.html";
 });
+
+/*Omdirigera användare vid klick på menyknapp*/
+const menuBtn = document.querySelector("#menuBtn");
+
+bookAnimation.addEventListener("click", () => {
+    window.location.href = "/dinner-menu.html";
+});
